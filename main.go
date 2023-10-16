84bc8efa2d45c8fbedcd9b8ffe16a0646de1f46f
jmccallum-rbi <jamiemccallum0@gmail.com>
2023-10-16 22:25:52 +0100

    adding in a content header

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my first site!</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func main() {
