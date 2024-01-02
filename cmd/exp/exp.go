b1a68e32adc8ff714761f9a654d49620ad2bb5fc
NotMango <82413197+enlistedmango@users.noreply.github.com>
2024-01-02 16:58:55 +0000

    working on some example templates

---
+++ cmd/exp/exp.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Bio  string
+	Age  int
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		Name: "Jamie Smith",
+		Bio:  `<script>alert("Haha, you have been h4x0r3d!");</script>`,
+		Age:  123,
+	}
+
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+}
